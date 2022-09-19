b6e4e29a6e485d87c47399fd3551ae1e49a55406
Spark <79936503+spa5k@users.noreply.github.com>
2022-09-20 00:06:48 +0530

    feat(styles): ✨ added modal for trailer

--- src/solid/Modal.jsx
+++ src/solid/Modal.jsx
@@ -34,30 +34,50 @@ export const Modal = () => {
         focusElementOnOpen="menuPopup"
       >
         <div
-          class="modal-container bg-white"
+          class="modal-container fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-blend-normal bg-black backdrop-blur-3xl bg-gray-500/30"
           onClick={onClickOverlay}
           role="presentation"
         >
           <div
-            class="modal"
+            class="relative w-auto p-5 bg-gray-500/30  items-end flex flex-col "
             role="dialog"
             aria-modal="true"
-            // tabindex="-1"
+            tabindex="-1"
             ref={btnSaveEl}
           >
-            <h4>Modal Text</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <div class="close-btns">
-              <button onClick={onClickClose}>Cancel</button>
-              <button onClick={onClickClose}>Save</button>
-            </div>
             <button
-              class="x-btn"
+              class="p-2 m-2 bg-aquamarine"
               aria-label="close modal"
               onClick={onClickClose}
             >
               ✕
             </button>
+
+            <iframe
+              frameborder="0"
+              scrolling="no"
+              marginheight="0"
+              marginwidth="0"
+              width="788.54"
+              height="443"
+              type="text/html"
+              src="https://www.youtube.com/embed/RpmqwNaGMyM?autoplay=1&fs=1&iv_load_policy=3&showinfo=1&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"
+            >
+              <div>
+                <small>
+                  <a href="https://youtubeembedcode.com/de/">
+                    youtubeembedcode.com/de/
+                  </a>
+                </small>
+              </div>
+              <div>
+                <small>
+                  <a href="https://bettingutanspelpaus.co/">
+                    bettingutanspelpaus
+                  </a>
+                </small>
+              </div>
+            </iframe>
           </div>
         </div>
       </Dismiss>
