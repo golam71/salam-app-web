382f5d175b375fcea80f40051a024288b540da9b
Golam Moula <74001687+golam71@users.noreply.github.com>
2023-01-22 20:56:51 +0600

    Update Navbar.jsx

--- src/solid/Navbar.jsx
+++ src/solid/Navbar.jsx
@@ -9,7 +9,7 @@ export const NavBar = (props) => {
           <div class="flex items-center">
             <div class="flex-shrink-0">
               <a href="/" aria-label="Link to homepage">
-                <img class="h-[45px]" src="../../public/favicon.svg" alt="salam app logo" />
+                <img class="h-[45px]" src="/favicon.svg" alt="salam app logo" />
               </a>
             </div>
           </div>
